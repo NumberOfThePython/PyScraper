763f3918e33828c0b033a6e79d7c3c31ed732de5
Kamil Sawicki <KamilSawicki@PL-KSAWICKIMB.local>
2024-12-11 11:56:03 +0100

    dokonczenie funkcji przycisku

--- Desktop/WebPyProject/main.py
+++ Desktop/WebPyProject/main.py
@@ -4,10 +4,21 @@ from dead_linker import check_dead_link
 
 # FUNKCJA do wpisania dla przycisku "sprawdź linki":
 def on_check_link():
-    url = url_entry.get()
-    if url:
-        result = check_dead_link(url)
-        messagebox.showinfo("Status", result)
-    else:
+    url = url_entry.get() #pobranie URL z pola URL_ENTRY DO ZDEFINIOWANIA JAKO POLE
+    if not url:
         messagebox.showwarning("Podaj URL.")
+        return
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 404:
+            status.label.config(text="Błąd 404")
+        elif response.status_code == 403:
+            status.label.config(text="Bład 403 [Forbidden]")
+        elif response.status.code == 500:
+            status.label.config(text="Błąd 500 [Internal Server Error]")
+        else
+            status.label.config(text=f"Link jest OK (Status: {response.status_code})")
+    except requests.exceptions.RequestException as e:
+        status_label.config(text=f"Błąd: {e}")
 
